50bf32bf8d3d7a25cc17aa8e16fc860dac59417b
Bogdan-K-A <kozhoma_b@ukr.net>
2021-12-02 23:06:28 +0200

    new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+class App extends Component() {
+  render() {
+    return (
+      <div>
+        <p>dddddd</p>
+      </div>
+    )
+  }
+}
+
+// function App() {
+//   return <h2>hello</h2>
+// }
+
+export default App
