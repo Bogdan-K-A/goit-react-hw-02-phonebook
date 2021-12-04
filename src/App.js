7cf5e9d8c0e5d1d02895454699ea43cb658a3ef7
Bogdan-K-A <kozhoma_b@ukr.net>
2021-12-04 23:26:49 +0200

    2dz

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
 import { Component } from 'react'
+import shortid from 'shortid'
+import Container from './components/container/Container'
+import { ContactList } from './components/ContactList/ContactList'
+import ContactForm from './components/ContactForm/ContactForm'
+import { ContactFilter } from './components/ContactFilter/ContactFilter'
 
-class App extends Component() {
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  }
+
+  /* ---------------------- добавление контакта в список ---------------------- */
+  addContact = (name, number) => {
+    const contact = {
+      name,
+      number,
+      id: shortid.generate(),
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }))
+  }
+
+  /* ---------------------------- Фильтр контактов ---------------------------- */
+  contactsFilter = (e) => {
+    this.setState({ filter: e.target.value })
+  }
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state
+    const normalizedFilter = filter.toLowerCase()
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    )
+  }
+
   render() {
+    const { filter } = this.state
+    const visibleContacts = this.getVisibleContacts()
+
     return (
-      <div>
-        <p>dddddd</p>
-      </div>
+      <Container>
+        <h1>Phoneboock</h1>
+        <ContactForm onAddContact={this.addContact} />
+        <h2>Contacts</h2>
+
+        <ContactFilter value={filter} onChange={this.contactsFilter} />
+        <ContactList contacts={visibleContacts} />
+      </Container>
     )
   }
 }
-
-// function App() {
-//   return <h2>hello</h2>
-// }
 
 export default App
